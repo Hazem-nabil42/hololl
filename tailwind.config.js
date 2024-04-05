fe5b3bcacc3ca99c9b13206b013f559b3820d66c
Hazem-nabil42 <hazem.alieldin@ejust.edu.eg>
2024-04-05 16:38:28 +0200

    Updated files with new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,10 +27,10 @@ module.exports = {
         accent: '#F13024',
       },
       backgroundImage: {
-        explosion: 'url("./bg-explosion.png")',
-        circles: 'url("./bg-circles.png")',
-        circleStar: 'url("./circle-star.svg")',
-        site: 'url("./site-bg.svg")',
+        explosion: 'url("./public/bg-explosion.png")',
+        circles: 'url("./public/bg-circles.png")',
+        circleStar: 'url("./public/circle-star.svg")',
+        site: 'url("./public/site-bg.svg")',
         header: 'url("https://64.media.tumblr.com/2d12e193489c59d83770612606c483a8/b455a323a3a051a5-f7/s400x600/0808f8d8a2a44c4ec38700c1b6063e38c9f34bd5.gif")',
       },
       animation: {
